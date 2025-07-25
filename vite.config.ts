936b9ed74691f0dc085ca5dce67e5ef93ef5f016
Gabriel Abreu <gabriel@gabbium.dev>
2025-07-25 16:32:19 -0300

    feat: cleanup and prettier setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import tsConfigPaths from "vite-tsconfig-paths";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts({
+      tsconfigPath: "./tsconfig.app.json",
+      exclude: ["**/*.test.@(js|jsx|mjs|ts|tsx)", "**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+    }),
+    tsConfigPaths({
+      projects: ["./tsconfig.app.json"],
+    }),
+  ],
+});
